5c498d2a71bb86a38bae6107cabe770f7c52cbbd
suliar <gaucho4utd@hotmail.com>
2020-03-29 14:07:09 +0100

    create blog proto added

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/suliar/grpc-go-course/greet/greetpb"
+	"github.com/suliar/grpc-go-course/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 )
 
-type server struct {}
+var collection *mongo.Collection
+
+type server struct{}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	// if we crash the go code, we get the file name and line number
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	fmt.Println("connecting to Mongodb")
+	// connect to MongoDB
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// create new Database and collection
+	collection = client.Database("mydb").Collection("blog")
+
+	fmt.Println("Blog Service Started")
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
@@ -19,9 +52,26 @@ func main() {
 
 	var opts []grpc.ServerOption
 	s := grpc.NewServer(opts...)
-	greetpb.RegisterGreetServiceServer(s, &server{})
+	blogpb.RegisterBlogServiceServer(s, &server{})
 
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	go func() {
+		fmt.Println("Starting server")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	// wait for control C to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// block until signal is received
+	<-ch
+	fmt.Println("Stopping the server")
+	s.Stop()
+	fmt.Println("Closing the listener")
+	_ = lis.Close()
+	fmt.Println("Closing Mongodb connection")
+	_ = client.Disconnect(context.TODO())
+	fmt.Println("End of Program")
 }
